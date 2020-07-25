c5777715a8f5dd07e9f04bbccf4ae243a99abac1
Angel Octavio Lopez <a.o.lopez.cruz@gmail.com>
2020-07-25 00:27:02 -0500

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# App en React de Criptomonedas
-
-## Pequeña app en React que obtiene el valor de criptomonedas
-
-### Aplicacion para aprender el uso de llamado a APIs
-
-Iconos diseñados por [Eucalyp](https://www.flaticon.es/autores/eucalyp) from [www.flaticon.es](https://www.flaticon.es/)
-
---
+++ precache-manifest.32fbcbc5bbed040ec1a6be85ea77cbee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d56e3d32877acc4d6607ac2b977b34",
+    "url": "/AppCriptomonedas/index.html"
+  },
+  {
+    "revision": "025fd0bf9b713773060d",
+    "url": "/AppCriptomonedas/static/css/main.d0dd5b03.chunk.css"
+  },
+  {
+    "revision": "df21931d89fc09bf47bf",
+    "url": "/AppCriptomonedas/static/js/2.6c57615d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AppCriptomonedas/static/js/2.6c57615d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "025fd0bf9b713773060d",
+    "url": "/AppCriptomonedas/static/js/main.6b8afa36.chunk.js"
+  },
+  {
+    "revision": "f99108c7e28b99f281a1",
+    "url": "/AppCriptomonedas/static/js/runtime-main.c48705c8.js"
+  },
+  {
+    "revision": "c52253959ea2ebb9aa64148b80782558",
+    "url": "/AppCriptomonedas/static/media/criptomonedas.c5225395.svg"
+  }
+]);
